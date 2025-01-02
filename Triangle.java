104ae932a1ea9112d1672f6ec38abe62b7672e98
BT_2S14_05 <baojolie@gmail.com>
2025-01-02 10:04:16 -0500

    Triangle constructor and instance variables created

--- Triangle.java
+++ Triangle.java
@@ -1,2 +1,14 @@
 public class Triangle {
+    private int base;
+    private int height;
+
+    public Triangle (int b, int h){
+        base = b;
+        height = h;
+    }
+
+    public Triangle (){
+        base = 6;
+        height = 6;
+    }
 }
